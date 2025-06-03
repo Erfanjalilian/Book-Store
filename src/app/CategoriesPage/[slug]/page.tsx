b78d86fd280c6d644ad0a98a39eed4ef9e823c63
erfanjalilian <erfanjaliliane995@gmail.com>
2025-06-03 20:50:02 +0330

    a

--- src/app/CategoriesPage/[slug]/page.tsx
+++ src/app/CategoriesPage/[slug]/page.tsx
@@ -1,13 +1,14 @@
+import type { Metadata } from 'next'
 import HandelSlug from "@/app/Components/HandelSlug/page"
 
-type Props = {
+type PageProps = {
   params: {
     slug: string
   }
   searchParams?: { [key: string]: string | string[] | undefined }
 }
 
-export default function ProductPage({ params }: Props) {
+export default function ProductPage({ params }: PageProps) {
   return (
     <div>
       <HandelSlug slug={params.slug} />
