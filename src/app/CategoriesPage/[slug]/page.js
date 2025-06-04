1d3cb145396bb3f7279ce4485bab65bcc683afc7
erfanjalilian <erfanjaliliane995@gmail.com>
2025-06-04 16:51:32 +0330

    my hope and hope

--- src/app/CategoriesPage/[slug]/page.js
+++ src/app/CategoriesPage/[slug]/page.js
@@ -10,22 +10,3 @@ export default function ProductPage({ params }) {
   )
 }
 
-// ✅ اضافه کردن generateStaticParams برای جلوگیری از ارور Netlify و TypeScript
-export async function generateStaticParams() {
-  try {
-    const res = await fetch("https://683dbdd7199a0039e9e6b54e.mockapi.io/Products")
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch products")
-    }
-
-    const products= await res.json()
-
-    return products.map((product) => ({
-      slug: product.id,
-    }))
-  } catch (error) {
-    console.error("Error in generateStaticParams:", error)
-    return [] // یا می‌تونی مقادیر تستی بذاری
-  }
-}
