cf1b1c0ddca59cc56adf84b68bc57058a0fd40ef
erfanjalilian <erfanjaliliane995@gmail.com>
2025-07-21 11:26:19 +0330

    debog the image

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,9 @@ const nextConfig: NextConfig = {
       'novinhub.com',
       'deema.agency',
       'dkstatics-public.digikala.com',
-      'tarh.ir', // ← دامنه موردنظر شما اضافه شد
-      '8tagketab.ir'
+      'tarh.ir',
+      '8tagketab.ir',
+      'i.ibb.co' // ← این دامنه برای نمایش تصاویر imgbb اضافه شد
     ],
   },
 }
